8b6075f2199c4b9102b96f30730ca6393ccc38df
Harsh <harshlhagency@gmail.com>
2026-01-08 14:51:43 +0530

    added gtm ga gad

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -10,8 +11,54 @@ const poppins = Poppins({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+
+    <>
+          {/* Google Analytics */}
+          <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-VX06MFX8TB"
+        strategy="afterInteractive"
+      />
+      <Script id="ga-script" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-VX06MFX8TB');
+        `}
+      </Script>
+
+      {/* Google Ads Conversion */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17726157177"
+        strategy="afterInteractive"
+      />
+      <Script id="ads-script" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-17726157177');
+        `}
+      </Script>
+
+      {/* Google Tag Manager */}
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PZ26KTWS');
+        `}
+      </Script>
     <div className={poppins.className}>
       <Component {...pageProps} />
     </div>
+    </>
+    
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-
 export default function Document() {
-
   return (
     <Html lang="en">
       <Head>
@@ -10,20 +8,57 @@ export default function Document() {
 
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
 
         {/* Apple */}
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon.png"
+          sizes="180x180"
+        />
 
         {/* Android / PWA */}
-        <link rel="icon" type="image/png" sizes="192x192" href="/icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/icon-512x512.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/icon-512x512.png"
+        />
 
-        {/* Theme color (optional but professional) */}
+        {/* Theme color */}
         <meta name="theme-color" content="#FFBB00" />
       </Head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-PZ26KTWS"
+                height="0"
+                width="0"
+                style="display:none;visibility:hidden"
+              ></iframe>
+            `,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
